16a46945aa54414fbc9b6d81aaa5be537ebd1f84
Alexander Blom <me@alexanderblom.se>
2012-07-29 17:21:42 +0200

    Move TransformingLoader to its own file

--- webimageloader/src/com/webimageloader/loader/LoaderManager.java
+++ webimageloader/src/com/webimageloader/loader/LoaderManager.java
@@ -1,7 +1,5 @@
 package com.webimageloader.loader;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -10,7 +8,6 @@ import java.util.List;
 import com.webimageloader.transformation.Transformation;
 
 import android.graphics.Bitmap;
-import android.util.Log;
 
 public class LoaderManager {
     private MemoryCache memoryCache;
@@ -103,61 +100,6 @@ public class LoaderManager {
     private static <T> void add(List<T> list, T item) {
         if (item != null) {
             list.add(item);
-        }
-    }
-
-    private static class TransformingLoader implements Loader {
-        private static final String TAG = "TransformingLoader";
-
-        @Override
-        public void load(LoaderRequest request, Iterator<Loader> chain, final Listener listener) {
-            Log.d(TAG, "Transforming " + request);
-
-            final Transformation transformation = request.getTransformation();
-
-            // Modify request
-            LoaderRequest modified = request.withoutTransformation();
-            chain.next().load(modified, chain, new Listener() {
-                @Override
-                public void onStreamLoaded(InputStream is, Metadata metadata) {
-                    try {
-                        Bitmap b = transformation.transform(is);
-                        deliverResult(b, metadata);
-                    } catch (IOException e) {
-                        listener.onError(e);
-                    }
-                }
-
-                @Override
-                public void onBitmapLoaded(Bitmap b, Metadata metadata) {
-                    b = transformation.transform(b);
-                    deliverResult(b, metadata);
-                }
-
-                private void deliverResult(Bitmap b, Metadata metadata) {
-                    if (b == null) {
-                        onError(new NullPointerException("Transformer returned null"));
-                    } else {
-                        listener.onBitmapLoaded(b, metadata);
-                    }
-                }
-
-                @Override
-                public void onNotModified(Metadata metadata) {
-                    listener.onNotModified(metadata);
-                }
-
-                @Override
-                public void onError(Throwable t) {
-                    listener.onError(t);
-                }
-            });
-        }
-
-        @Override
-        public void cancel(LoaderRequest request) {
-            // We can't cancel anything as we don't run the
-            // transformation on our own thread
         }
     }
 }
