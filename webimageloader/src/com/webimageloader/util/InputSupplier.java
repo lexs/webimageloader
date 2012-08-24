5d9643858ea40535d97e0eafb37e31c15d7d13a3
Alexander Blom <me@alexanderblom.se>
2012-08-24 12:38:09 +0200

    Add javadoc to InputSupplier

--- webimageloader/src/com/webimageloader/util/InputSupplier.java
+++ webimageloader/src/com/webimageloader/util/InputSupplier.java
@@ -3,6 +3,18 @@ package com.webimageloader.util;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * A factory of {@link InputStream}s
+ * 
+ * @author Alexander Blom <alexanderblom.se>
+ */
 public interface InputSupplier {
+    /**
+     * Open a new {@link InputStream}, you should be sure to close any previously
+     * opened streams before
+     * 
+     * @return a fresh stream
+     * @throws IOException if opening the stream failed
+     */
     InputStream getInput() throws IOException;
 }
