09c817fe879adf701eaec6fe0c3041ca405713c1
Alexander Blom <me@alexanderblom.se>
2013-01-09 19:44:13 +0100

    Add base ImageLoader
    Conflicts:
    
    	webimageloader/src/main/java/com/webimageloader/ImageLoaderImpl.java
    	webimageloader/src/main/java/com/webimageloader/ext/ListImageLoader.java

--- webimageloader/src/main/java/com/webimageloader/ImageLoaderImpl.java
+++ webimageloader/src/main/java/com/webimageloader/ImageLoaderImpl.java
@@ -12,9 +12,10 @@ import android.os.Message;
 import com.webimageloader.loader.LoaderManager;
 import com.webimageloader.loader.MemoryCache;
 import com.webimageloader.transformation.Transformation;
+import com.webimageloader.util.AbstractImageLoader;
 import com.webimageloader.util.WaitFuture;
 
-class ImageLoaderImpl implements ImageLoader {
+class ImageLoaderImpl extends AbstractImageLoader {
     private LoaderManager loaderManager;
     private HandlerManager handlerManager;
 
@@ -38,16 +39,6 @@ class ImageLoaderImpl implements ImageLoader {
     @Override
     public MemoryCache getMemoryCache() {
         return loaderManager.getMemoryCache();
-    }
-
-    @Override
-    public Bitmap loadBlocking(String url) throws IOException {
-        return loadBlocking(new Request(url));
-    }
-
-    @Override
-    public Bitmap loadBlocking(String url, Transformation transformation) throws IOException {
-        return loadBlocking(new Request(url, transformation));
     }
 
     @Override
@@ -97,32 +88,10 @@ class ImageLoaderImpl implements ImageLoader {
         }
     }
 
-    @Override
-    public void preload(String url) {
-        preload(new Request(url));
-    }
-
-    @Override
-    public void preload(String url, Transformation transformation) {
-        preload(new Request(url, transformation));
-    }
-
-    @Override
     public void preload(Request request) {
         loadInternal(null, request, null);
     }
 
-    @Override
-    public <T> Bitmap load(T tag, String url, Listener<T> listener) {
-        return load(tag, new Request(url), listener);
-    }
-
-    @Override
-    public <T> Bitmap load(T tag, String url, Transformation transformation, Listener<T> listener) {
-        return load(tag, new Request(url, transformation), listener);
-    }
-
-    @Override
     public <T> Bitmap load(T tag, Request request, Listener<T> listener) {
         return loadInternal(tag, request, handlerManager.getListener(tag, listener));
     }
---
+++ webimageloader/src/main/java/com/webimageloader/util/AbstractImageLoader.java
@@ -0,0 +1,40 @@
+package com.webimageloader.util;
+
+import android.graphics.Bitmap;
+import com.webimageloader.ImageLoader;
+import com.webimageloader.Request;
+import com.webimageloader.transformation.Transformation;
+
+import java.io.IOException;
+
+public abstract class AbstractImageLoader implements ImageLoader {
+    @Override
+    public Bitmap loadBlocking(String url) throws IOException {
+        return loadBlocking(new Request(url));
+    }
+
+    @Override
+    public Bitmap loadBlocking(String url, Transformation transformation) throws IOException {
+        return loadBlocking(new Request(url, transformation));
+    }
+
+    @Override
+    public void preload(String url) {
+        preload(new Request(url));
+    }
+
+    @Override
+    public void preload(String url, Transformation transformation) {
+        preload(new Request(url).withTransformation(transformation));
+    }
+
+    @Override
+    public <T> Bitmap load(T tag, String url, Listener<T> listener) {
+        return load(tag, new Request(url), listener);
+    }
+
+    @Override
+    public <T> Bitmap load(T tag, String url, Transformation transformation, Listener<T> listener) {
+        return load(tag, new Request(url, transformation), listener);
+    }
+}
