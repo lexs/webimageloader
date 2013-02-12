ed042081528dfa19f45e2b00114bdec73f05db5c
Alexander Blom <me@alexanderblom.se>
2013-02-12 14:05:04 +0100

    Increase default number of threads

--- webimageloader/src/main/java/com/webimageloader/Constants.java
+++ webimageloader/src/main/java/com/webimageloader/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final long MAX_AGE_NOT_FORCED = -1;
 
     public static final int DEFAULT_DISK_THREADS = 1;
-    public static final int DEFAULT_NETWORK_THREADS = 2;
+    public static final int DEFAULT_NETWORK_THREADS = 4;
 
     public static final Bitmap.CompressFormat DEFAULT_COMPRESS_FORMAT = Bitmap.CompressFormat.JPEG;
     public static final int DEFAULT_COMPRESS_QUALITY = 75;
