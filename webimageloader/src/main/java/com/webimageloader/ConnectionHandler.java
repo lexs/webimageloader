49b0fc6cbe207412552d4f0cc878f03764815c78
Christopher Orr <chris@iosphere.de>
2013-02-27 20:06:03 +0100

    Allow users to specify a custom HttpURLConnection.
    As discussed in issue #11, this allows the use of libraries
    like OkHttp to work around various native Android HTTP bugs.

--- webimageloader/src/main/java/com/webimageloader/ConnectionHandler.java
+++ webimageloader/src/main/java/com/webimageloader/ConnectionHandler.java
@@ -1,19 +1,23 @@
 package com.webimageloader;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
- * Interface for accessing a {@link HttpURLConnection} before a request is
- * made, this can be used for example doing authentication. This handler is
- * only used for http(s) connections.
+ * Interface for creating an {@link HttpURLConnection} for a request.
+ *
+ * This can be used, for example, for doing HTTP authentication or using a custom HttpURLConnection.
+ * This handler is only used for http(s) connections.
  *
  * @author Alexander Blom <alexanderblom.se>
  */
 public interface ConnectionHandler {
     /**
-     * Called before a request. Note that this method is called from a
-     * background thread.
-     * @param connection the connection
+     * Called before a request. Note that this method is called from a background thread.
+     *
+     * @param url URL to be loaded.
+     * @return The HTTP connection object to be used for this URL.
      */
-    void handleConnection(HttpURLConnection connection);
+    HttpURLConnection handleConnection(URL url) throws IOException;
 }
